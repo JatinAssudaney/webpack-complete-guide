bad110ffa840a873f704553336401facc965287d
Jatin Assudaney <jatin.assudaney24@gmail.com>
2024-12-14 14:50:32 +0530

    (add): loaders in webpack

---
+++ 03_loaders/src/index.js
@@ -0,0 +1,7 @@
+import addImage from "./add-image.js";
+import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
+
+// addImage();
+
+const helloWorld = new HelloWorldButton();
+helloWorld.render();
---
+++ 03_loaders/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "dist/",
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(ttf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(jpg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-transform-class-properties"],
+          },
+        },
+      },
+    ],
+  },
+};
